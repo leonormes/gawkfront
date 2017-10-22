5ed7c9ca3dba7ae9153229f991c002b8623f5203
Leon Ormes <leonormes@gmail.com>
2017-10-22 10:25:43 +0100

    init root commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    app: path.join(__dirname, 'src'),
+    build: path.join(__dirname, '../server/src/public')
+};
+
+module.exports = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Seaview Register'
+        })
+    ]
+};
