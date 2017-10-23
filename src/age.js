a23096ba5a6e0b14fdd642e7bd52d746cfe39c41
Leon Ormes <leonormes@gmail.com>
2017-10-23 07:13:13 +0100

    refactor
    There are now collections of all pupils and current pupils

--- src/age.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-
-export default function (dob) {
-	let a = moment();
-	let b = moment(dob);
-	return a.diff(b, 'months');
-}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,86 @@
 const pupilTemplate = require('./views/row.handlebars')
 const moment = require('moment');
+let pupilRecordsArray;
+let activePupils;
 
 fetch('http://localhost:8081/allPupils')
     .then((res) => {
         return res.json();
     })
-    .then((elm) => {
+    .then((pupils) => {
+        console.log(pupils)
+    pupilRecordsArray = pupilRecordsToArray(pupils);
+    console.log('pupilrecordsArray', pupilRecordsArray)
+    activePupils = filterPupils(pupilRecordsArray);
+        return activePupils;
+    })
+    .then((activePupils) => {
         app = document.getElementsByClassName('app')[0];
-
-        Object.keys(elm).forEach((r) => {
+        console.log(activePupils)
+        activePupils.forEach((pupil) => {
         let divasync = document.createElement('div');
-            if (elm.hasOwnProperty(r)) {
                 divasync.innerHTML += pupilTemplate({
-                id: r,
-                adultfname: elm[r].adultfname,
-                adultsurname: elm[r].adultsurname,
-                allergies: elm[r].allergies,
-                childfname: elm[r].childfname,
-                childsurname: elm[r].childsurname,
-                age: getPupilAge(elm[r].dob),
-                email: elm[r].email,
-                hasStarted: elm[r].hasStarted,
-                phone: elm[r].phone,
-                startDate: moment(elm[r].startDate).format('MMMM Do YYYY'),
-                status: elm[r].status,
-                dateAdded: moment(elm[r].timeStamp).format('MMMM Do YYYY')
+                id: pupil.key,
+                adultfname: pupil['adultfname'],
+                adultsurname: pupil['adultsurname'],
+                allergies: pupil['allergies'],
+                childfname: pupil['childfname'],
+                childsurname: pupil['childsurname'],
+                age: getPupilAge(pupil['dob']),
+                email: pupil['email'],
+                hasStarted: pupil['hasStarted'],
+                phone: pupil['phone'],
+                startDate: moment(pupil['startDate']).format('MMMM Do YYYY'),
+                status: pupil['status'],
+                dateAdded: moment(pupil['timeStamp']).format('MMMM Do YYYY')
             });
-        }
 
         app.appendChild(divasync);
+        $('#' + pupil.key).find('.card-footer')
+        .click((e) => {
+            handleFooterClick(e);
+        });
         })
     })
 
 function getPupilAge(dob) {
     let a = moment();
     let b = moment(dob);
-    return a.diff(b, 'months');
+    return a.diff(b, 'months')
 };
 
 function editRecord(id) {
     console.log(id)
+}
+
+function handleFooterClick(e) {
+    if($(e.target).hasClass('edit')) {
+        console.log('Edit ' + $(e.target).parent().parent().parent().attr('id'))
+    } else if ($(e.target).hasClass('remove')) {
+        console.log('Delete ' + $(e.target).parent().parent().parent().attr('id'))
+    }
+}
+
+/**
+ * pupilRecordsToArray - It is easier to sort and manipulate the
+ * data in an array.
+ * This function takes the Object returned from the server and
+ * maps the data to an array of objects.
+ * @param {Object} pupilRecord - The Object to be converted to Array
+ * @returns {undefined}
+ */
+function pupilRecordsToArray(pupilRecord) {
+    const pupilRecordsArray = Object.keys(pupilRecord).map(function(key) {
+        pupilRecord[key].key = key;
+        return pupilRecord[key];
+    });
+    return pupilRecordsArray;
+}
+function filterPupils(pupils) {
+    let filtered = pupils.filter(function(p) {
+        return p.status === 'Active' || p.status === 'Waiting';
+
+    })
+    return filtered;
+
 }
