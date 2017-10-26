3c201f56f41cf269c479720d8acc18f7c519ed75
Leon Ormes <leonormes@gmail.com>
2017-10-26 15:25:35 +0100

    websocket file

--- src/websocket.js
+++ src/websocket.js
@@ -3,12 +3,12 @@
   const socket = new WebSocket('ws://localhost:3322');
 
   // Connection opened
-  socket.addEventListener('open', function (event) {
+  socket.addEventListener('open', function(event) {
     console.log('Socket open!!');
   });
 
   // Listen for messages
-  socket.addEventListener('message', function (event) {
+  socket.addEventListener('message', function(event) {
     console.dir(event);
     console.log('Message from server ', event.data);
   });
